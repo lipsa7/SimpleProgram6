f8f5165a2dc6f1942351bc45a2956390435c2eca
lipsa7 <lipsakar98@gmail.com>
2021-07-28 12:21:06 +0530

    Add project files.

---
+++ Rdppractice3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rdppractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(UserProgramCode.calculateDiff(arr));
+
+            Console.ReadKey();
+        }
+    }
+
+    class UserProgramCode
+    {
+        public static int calculateDiff(int[] arr)
+        {
+            int n = arr.Length;
+
+            int max = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            int min = 100;
+            //assuming numbers in array are less than 100
+
+            for(int i = 0; i < n; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            return (max - min);
+        }
+    }
+}
